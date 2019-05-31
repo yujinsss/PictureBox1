ea947ca7bd05fd15be3b86b6478f4e46f5bb7f3d
Yujin Lee <lyj62346972@gmail.com>
2019-05-31 11:48:24 +0900

    #2 picturebox, imagelist 컨트롤을 사용하여 시진첩 코드 구현

--- PictureBox1/Form1.Designer.cs
+++ PictureBox1/Form1.Designer.cs
@@ -55,6 +55,7 @@
             this.btnPrev.TabIndex = 1;
             this.btnPrev.Text = "이전";
             this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.BtnPrev_Click);
             // 
             // btnNext
             // 
@@ -64,6 +65,7 @@
             this.btnNext.TabIndex = 2;
             this.btnNext.Text = "다음";
             this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.BtnNext_Click);
             // 
             // lblNum
             // 
@@ -136,6 +138,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "사진첩";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.picImg)).EndInit();
             this.ResumeLayout(false);
 
--- PictureBox1/Form1.cs
+++ PictureBox1/Form1.cs
@@ -11,9 +11,37 @@ namespace PictureBox1
 {
     public partial class Form1 : Form
     {
+        int imgCount = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.picImg.Image = this.imgList.Images[imgCount];
+            this.lblNum.Text = (this.imgCount + 1) + "/" + this.imgList.Images.Count;
+        }
+
+        private void BtnNext_Click(object sender, EventArgs e)
+        {
+            this.imgCount++;
+            if (imgCount >= this.imgList.Images.Count) {
+                imgCount = 0;
+            }
+            this.picImg.Image = this.imgList.Images[imgCount];
+            this.lblNum.Text = (this.imgCount + 1) + "/" + this.imgList.Images.Count;
+        }
+
+        private void BtnPrev_Click(object sender, EventArgs e)
+        {
+            this.imgCount--;
+            if (imgCount < 0)
+            {
+                imgCount = this.imgList.Images.Count-1;
+            }
+            this.picImg.Image = this.imgList.Images[imgCount];
+            this.lblNum.Text = (this.imgCount + 1) + "/" + this.imgList.Images.Count;
         }
     }
 }
